b617f002e1f9d56164ae07ac614e2808604a310b
C-Vieira <caiovieira.official@gmail.com>
2024-09-24 14:48:43 -0300

    Tela Adicionar Lista

--- app/src/main/java/com/example/applistadecompras/ListAddActivity.kt
+++ app/src/main/java/com/example/applistadecompras/ListAddActivity.kt
@@ -1,7 +1,10 @@
 package com.example.applistadecompras
 
 import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.applistadecompras.databinding.ListAddViewBinding
 
 class ListAddActivity: Activity() {
@@ -12,5 +15,15 @@ class ListAddActivity: Activity() {
 
         binding = ListAddViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val placeholderUrl = "https://i0.wp.com/espaferro.com.br/wp-content/uploads/2024/06/placeholder-103.png?ssl=1"
+        val newShoppingList = ShoppingList(" ", null)
+
+        Glide.with(binding.root)
+            .load(newShoppingList.imageUrl ?: placeholderUrl)
+            .error(placeholderUrl)
+            .placeholder(ColorDrawable(Color.LTGRAY))
+            .centerCrop()
+            .into(binding.listImageView)
     }
 }
